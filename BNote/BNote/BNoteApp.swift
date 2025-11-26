d9bad23f637c85979439ec8d3cdf16c81adb834a
Prakash Tamang <pratmg2@gmail.com>
2025-11-25 21:50:55 -0600

    Added initial Xcode iPadOS project

---
+++ BNote/BNote/BNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BNoteApp.swift
+//  BNote
+//
+//  Created by faithNfunction on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
